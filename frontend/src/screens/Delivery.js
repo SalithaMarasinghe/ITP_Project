2537c49679567800f33936730f3fc31bcbac92a8
ThenulaHettige <mhtphettige@gmail.com>
2024-05-03 12:03:45 +0530

    Revert "commit"
    This reverts commit eb53d0219b6af566179af65a85ed6afc7bf6b74a.

--- frontend/src/screens/Delivery.js
+++ frontend/src/screens/Delivery.js
@@ -6,8 +6,6 @@ import Message from "../components/Message";
 import { useSelector, useDispatch } from "react-redux";
 import { updateDelivery, getOrderDetails } from "../redux/actions/orderActions";
 
-//test
-
 const DeliveryScreen = () => {
   
   let params = useParams();
