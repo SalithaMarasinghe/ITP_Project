e01b3c7b83a1c919ca6f5cf35f60e1711b3482f8
ThenulaHettige <mhtphettige@gmail.com>
2024-05-04 10:59:53 +0530

    Promotions PDF

--- frontend/src/screens/PromotionListScreen.js
+++ frontend/src/screens/PromotionListScreen.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Loading from "../components/Loading";
+import Button from 'react-bootstrap/Button';
+import { Row, Col, Table } from 'react-bootstrap';
+import Loading from '../components/Loading';
+import Message from '../components/Message';
 import { listPromotions, deletePromotion } from '../redux/actions/promotionActions';
 import { listProduct } from '../redux/actions/productActions';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
-const PromotionListScreen = ({ history }) => {
+const PromotionListScreen = () => {
   const dispatch = useDispatch();
 
   const promotionList = useSelector((state) => state.promotionList);
@@ -13,7 +18,6 @@ const PromotionListScreen = ({ history }) => {
 
   const productList = useSelector((state) => state.productList);
   const { products } = productList;
-
 
   useEffect(() => {
     dispatch(listPromotions());
@@ -23,72 +27,98 @@ const PromotionListScreen = ({ history }) => {
     dispatch(listProduct());
   }, [dispatch]);
 
-
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure you want to delete this promotion?')) {
       dispatch(deletePromotion(id));
     }
   };
 
-
   const getProductName = (productId) => {
-    const product = products.find(product => product._id === productId);
+    const product = products.find((product) => product._id === productId);
     return product ? product.name : '';
+  };
+
+  const generatePDF = () => {
+    const doc = new jsPDF();
+    doc.text('Promotion List', 14, 16);
+
+    doc.autoTable({
+      head: [['Name', 'Type', 'Value', 'Valid Period', 'Related Product']],
+      body: promotions.map((promotion) => [
+        promotion.name,
+        promotion.type,
+        promotion.value,
+        promotion.validPeriod,
+        getProductName(promotion.relatedProduct),
+      ]),
+    });
+
+    doc.save('promotion-list.pdf'); // Save the generated PDF
   };
 
   return (
     <div>
-      <div className="row align-items-center">
-        <div className="col">
+      <Row className="align-items-center">
+        <Col>
           <h1>Promotions</h1>
-        </div>
-       
-      </div>
+        </Col>
+        <Col className="text-right">
+          <Button onClick={generatePDF} variant="success">
+            Download PDF
+          </Button>
+        </Col>
+      </Row>
+
       {loading ? (
         <Loading />
       ) : error ? (
-        <p>{error}</p>
+        <Message variant="danger">{error}</Message>
       ) : (
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Value</th>
-              <th>Valid Period</th>
-              <th>Related Product</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {promotions.map((promotion) => (
-              <tr key={promotion._id}>
-                <td>{promotion.name}</td>
-                <td>{promotion.type}</td>
-                <td>{promotion.value}</td>
-                <td>{promotion.validPeriod}</td>
-                <td>{getProductName(promotion.relatedProduct)}</td>
-                <td>
-                  <Link to={`/admin/edit-promotion/${promotion._id}`} className="btn btn-primary mr-2">
-                    Edit
-                  </Link>
-                  <button onClick={() => deleteHandler(promotion._id)} className="btn btn-danger">
-                    Delete
-                  </button>
-                </td>
+        <div>
+          <Table striped hover className="table-sm">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Value</th>
+                <th>Valid Period</th>
+                <th>Related Product</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        
-      )}
-       <div className="col text-right">
-          <Link to="/admin/create-promotion" className="btn btn-primary">
-            Create Promotion
-          </Link>
+            </thead>
+            <tbody>
+              {promotions.map((promotion) => (
+                <tr key={promotion._id}>
+                  <td>{promotion.name}</td>
+                  <td>{promotion.type}</td>
+                  <td>{promotion.value}</td>
+                  <td>{promotion.validPeriod}</td>
+                  <td>{getProductName(promotion.relatedProduct)}</td>
+                  <td>
+                    <Link to={`/admin/edit-promotion/${promotion._id}`} className="btn btn-primary mr-2">
+                      Edit
+                    </Link>
+                    <Button
+                      variant="danger"
+                      size="sm"
+                      onClick={() => deleteHandler(promotion._id)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          <div className="text-right mt-3">
+            <Link to="/admin/create-promotion" className="btn btn-primary">
+              Create Promotion
+            </Link>
+          </div>
         </div>
+      )}
     </div>
-    
   );
 };
 
