729e99d33bf8fe35fae33e6db51eefec93c28122
NIpuna Pandithasundara <donipuna@gmail.com>
2024-05-07 11:21:47 +0530

    Update MyOrders.js

--- frontend/src/screens/MyOrders.js
+++ frontend/src/screens/MyOrders.js
@@ -4,22 +4,12 @@ import { Row, Col, Form, Button, Table } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import Loading from "../components/Loading";
 import Message from "../components/Message";
-import { updateProfile } from "../redux/actions/userActions";
 import { listMyOrders } from "../redux/actions/orderActions";
 
-const ProfileScreen = () => {
-  const [message, setMessage] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const dispatch = useDispatch();
+const MyOrders = () => {
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
-
-  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
-  const { successUpdated, loading, error } = userUpdateProfile;
 
   const orderListMy = useSelector((state) => state.orderListMy);
   const {
@@ -28,23 +18,7 @@ const ProfileScreen = () => {
     error: errorMyOrders,
   } = orderListMy;
 
-  useEffect(() => {
-    if (userInfo?.name) {
-      setName(userInfo.name);
-      setEmail(userInfo.email);
-      dispatch(listMyOrders());
-    }
-  }, [dispatch, userInfo]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      setMessage("Password do not match");
-    } else {
-      dispatch(updateProfile({ id: userInfo._id, name, email, password }));
-    }
-  };
-
+  
   return (
     <Row>
       
@@ -102,4 +76,4 @@ const ProfileScreen = () => {
   );
 };
 
-export default ProfileScreen;
+export default MyOrders;
