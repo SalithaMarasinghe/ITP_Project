1dc328a15f599bc934edd8422b3648a46297417a
NIpuna Pandithasundara <donipuna@gmail.com>
2024-05-07 09:30:32 +0530

    Revert "Fixed user profile details update Issue"
    This reverts commit 81b706fa158118613f7cd9272da95bad26044a32.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,10 +13,11 @@ import { protect, admin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(protect, admin, getUsers).put(protect, updateUser);
+router.route("/").get(protect, admin, getUsers);
 router
   .route("/:id")
   .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
   .delete(protect, admin, deleteUser);
 router.route("/login").post(authUser);
 router
